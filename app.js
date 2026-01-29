b696f16e20dd94455b8d1da5880924e547679eed
tidianewrdl <tidiandamadou01@gmail.com>
2026-01-29 16:52:00 +0000

    Update app.js
    tidiane bien
    bien

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 const crud = require('./crud.js');
 crud.addUser('Goku','Danty','kameha','goku123');
+crud.addUser('Vegeta','Prince','finalflash','vegeta123');
+crud.addUser('Piccolo','Namekian','specialbeamcannon','piccolo123');
