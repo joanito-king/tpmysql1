2b8395939938b7e853783986bf15e080ceb387dc
boveldy <kenganiboveldy@gmail.com>
2026-01-29 16:52:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const crud = require('./crud.js');
 crud.addUser('Goku','Danty','kameha','goku123');
 crud.addUser('alizee','ardo','nambé','csarr24');
+crud.addUser('mario','luigi','plombier','superluigi');
